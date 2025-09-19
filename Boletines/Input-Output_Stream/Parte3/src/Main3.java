0663a7bb9d538d3b943b1b56a51f4ac26a1eefc2
dam <samuelhermida55@gmail.com>
2025-09-19 14:42:37 +0200

    .

--- Boletines/Input-Output_Stream/Parte3/src/Main3.java
+++ Boletines/Input-Output_Stream/Parte3/src/Main3.java
@@ -1,17 +1,36 @@
 package Parte3.src;
 
+import java.io.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main3 {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws FileNotFoundException {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String ruta = "Input-Output_Stream/texto3.txt";
+        duplicarTexto(ruta);
+    }
+    static void duplicarTexto(String ruta) throws FileNotFoundException, RuntimeException {
+        DataInputStream in = new DataInputStream(new FileInputStream(ruta));
+        DataOutputStream out = new DataOutputStream(new FileOutputStream(ruta + ".dup"));
+        try {
+
+            String texto = in.readUTF();
+            System.out.println("mensaje de depuracion (Texto leido): " + texto);
+
+            System.out.println("o size e" + out.size());
+            texto = "a";
+            String r = texto + texto;
+            out.writeUTF(r);
+
+            System.out.println("mensaje de depuracion (Texto escrito): " + texto);
+
+            in.close();
+            out.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+
+
     }
 }
