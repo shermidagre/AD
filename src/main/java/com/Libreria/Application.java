514aae82366407260c1a9ef7d76278908930fb04
dam <samuelhermida55@gmail.com>
2025-10-02 10:30:42 +0200

    .

--- src/main/java/com/Libreria/Application.java
+++
@@ -1,8 +0,0 @@
-package com.Libreria;
-
-@SpringBootApplication
-public class Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
-}
