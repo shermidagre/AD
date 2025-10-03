d1b0a77e26900efa3519bb7825a2e7b302f547ea
dam <samuelhermida55@gmail.com>
2025-10-03 12:30:26 +0200

    .

--- PDT-AD/src/main/java/com/Libreria/services/interfaces/LibroService.java
+++
@@ -1,8 +0,0 @@
-// src/main/java/com/tu/proyecto/interfaces/LibroService.java
-package com.Libreria.services.interfaces;
-
-import com.Libreria.models.Libro;
-
-public interface LibroService {
-    Libro guardarLibro(Libro libro);
-}
